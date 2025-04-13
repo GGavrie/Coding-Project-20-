a0e92592169174b8a835639457f3e0fa003cef64
Gavrie Grant <gavrie@usf.edu>
2025-04-13 13:53:02 -0400

    Build TourCard with conditional rendering and interaction

--- src/components/TourCard.jsx
+++ src/components/TourCard.jsx
@@ -1,17 +1,31 @@
+// Import React hooks
 import { useState } from 'react';
 
+// TourCard component displays individual tour information
+// Props:
+// - id: unique identifier for the tour
+// - name: name of the tour
+// - info: detailed description of the tour
+// - image: URL of the tour image
+// - price: price of the tour
+// - onRemove: callback function to remove tour from list
 const TourCard = ({ id, name, info, image, price, onRemove }) => {
+  // State to toggle between showing full or truncated description
   const [readMore, setReadMore] = useState(false);
 
   return (
     <article className="tour-card">
+      {/* Tour image section with price overlay */}
       <div className="tour-img-container">
         <img src={image} alt={name} className="tour-img" />
         <p className="tour-price">${price}</p>
       </div>
+
+      {/* Tour details section */}
       <div className="tour-info">
         <h4>{name}</h4>
         <p>
+          {/* Show full text if readMore is true, otherwise show truncated version */}
           {readMore ? info : `${info.substring(0, 200)}...`}
           <button 
             className="read-more-btn"
@@ -20,6 +34,7 @@ const TourCard = ({ id, name, info, image, price, onRemove }) => {
             {readMore ? 'Show Less' : 'Read More'}
           </button>
         </p>
+        {/* Button to remove tour from list */}
         <button 
           className="btn btn-danger delete-btn"
           onClick={() => onRemove(id)}
